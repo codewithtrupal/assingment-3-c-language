2c7f17897510a6c6e8442c7b186b80300f5485ed
codewithtrupal <trupal122007@gmail.com>
2024-12-15 23:21:30 +0530

    q7

--- Q7.c
+++ Q7.c
@@ -8,6 +8,8 @@ struct state
     int percapitaincome;
 };
 
+void literacy_rate(struct state states[]);
+void percapita_income(struct state states[]);
 
 int main()
 {
@@ -15,16 +17,56 @@ int main()
 
     for (int i = 0; i < 5; i++)
     {
-        printf("State number : %d",i+1);
+        printf("State number : %d\n", i + 1);
 
         printf("Enter the state name : ");
         scanf("%s", states[i].name);
 
         printf("Enter the population : ");
-        scanf("%d", states[i].population);
+        scanf("%d", &states[i].population);
 
-        printf("")
+        printf("Enter the literacyrate : ");
+        scanf("%f", &states[i].literacyrate);
+
+        printf("Enter the per capita income : ");
+        scanf("%d", &states[i].percapitaincome);
     }
-    
+    literacy_rate(states);
 
+    percapita_income(states);
+    return 0;
+}
+
+void literacy_rate(struct state states[])
+{
+    int maxindex = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (states[i].literacyrate > states[maxindex].literacyrate)
+        {
+            maxindex = i;
+        }
+    }
+
+    printf("\nState with highest literacy rate\n");
+
+    printf("State name %s\n", states[maxindex].name);
+    printf("literacy rate %.2f\n", states[maxindex].literacyrate);
+}
+
+void percapita_income(struct state states[])
+{
+    int maxindex = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        if (states[i].percapitaincome > states[maxindex].percapitaincome)
+        {
+            maxindex = i;
+        }
+    }
+
+    printf("\n State with highest per capita income \n");
+
+    printf("State name %s\n", states[maxindex].name);
+    printf("Per capita income rate %d\n", states[maxindex].percapitaincome);
 }
